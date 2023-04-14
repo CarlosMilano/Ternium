f6d6d90431242ccaea2afd9549574bb1a1d69930
Erick Siller <102171276+Erick2929@users.noreply.github.com>
2023-04-13 18:35:26 -0600

    Test Commit for Vercel Deploy

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <p>Esta es la app de Ternium Talent</p>
+      <p>Esta es la app de Ternium Talent, esta vista esta en desarrollo</p>
     </>
   );
 }
