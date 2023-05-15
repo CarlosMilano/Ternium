8ef8f3e31b9cb10376d1036a3b0b76589d824e9a
oscarsotoo1 <oscarjesussoto@gmail.com>
2023-05-15 10:34:33 -0500

    Cloud db connection
    We connect the db from de cloud

--- pages/api/dbConfig.ts
+++ pages/api/dbConfig.ts
@@ -1,11 +1,18 @@
 import { Pool } from 'pg';
 
 const pool = new Pool({
-  user: 'postgres',
-  password: '1234',
-  host: 'localhost',
+  /*
+  user: 'adter01',
+  password: 'nn32BrziGP6wyLXBLmgRSOrlmfPV0O5v',
+  */
+  connectionString: 'postgres://adter01:nn32BrziGP6wyLXBLmgRSOrlmfPV0O5v@dpg-chh57gbhp8u065v23l4g-a.oregon-postgres.render.com/terniumtalentdb',
+  ssl: {
+    rejectUnauthorized: false
+  }
+  /*
   port: 5432,
-  database: 'ejemplo',
+  database: 'terniumtalentdb',
+  */
 });
 
 export default pool;
