845c118a32e810662e3bb7025f033f49eec45c1e
Jorge Becerril <110145796+jorgebecerrilgi@users.noreply.github.com>
2023-05-22 22:49:06 -0600

    Created types for the set of data on each table in the data base.

---
+++ utils/types/dbTables.ts
@@ -0,0 +1,38 @@
+export type TableEmpleado = {
+    id_empleado?: string;
+    nombre?: string;
+    edad?: number;
+    antiguedad?: number;
+    estudios?: string;
+    universidad?: string;
+    area_manager?: string;
+    direccion?: string;
+    puesto?: string;
+    pc_cat?: string;
+};
+export type TableTrayectoria = {
+    id_trayectoria?: string;
+    empresa?: string;
+    puesto?: string;
+    id_empleado?: string;
+};
+export type TableComentarios = {
+    id_comentario?: string;
+    nota?: number;
+    promedio_notas?: number;
+    comentario?: string;
+    id_empleado?: string;
+};
+export type TableResumen = {
+    id_resumen?: string;
+    resumen_perfil?: string;
+    id_empleado?: string;
+};
+export type TableEvaluacion = {
+    id_evaluacion?: string;
+    anio?: number;
+    performance?: number;
+    potencial?: number;
+    curva?: string;
+    id_empleado?: string;
+};
