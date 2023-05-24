79110ac9409b8fc6011b3ef4c6dd3d5c5dabd18e
Jorge Becerril <110145796+jorgebecerrilgi@users.noreply.github.com>
2023-05-24 17:12:02 -0600

    getTableEmpleado now recieves id_empleado is POST body to return a single employee.

--- pages/api/getTablaEmpleado.ts
+++ pages/api/getTablaEmpleado.ts
@@ -2,10 +2,20 @@ import { NextApiRequest, NextApiResponse } from "next";
 import getEmpleadoData from "./services/getTablaEmpleado";
 import { TableEmpleado } from "@/utils/types/dbTables";
 
+export interface GetEmpleadoRequestBody {
+    id_empleado: string;
+}
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse): Promise<void> {
     try {
-        const result: TableEmpleado[] = await getEmpleadoData();
-        res.status(200).json(result);
+        const { id_empleado }: GetEmpleadoRequestBody = req.body;
+        const result: TableEmpleado | null = await getEmpleadoData(id_empleado);
+
+        if (result) {
+            res.status(200).json(result);
+        } else {
+            res.status(404).json({ error: `Empleado with id_empleado = ${id_empleado} not found` });
+        }
     } catch (error) {
         console.error("Error fetching data:", error);
         res.status(500).json({ error: "Error fetching data" });
--- pages/api/services/getTablaEmpleado.ts
+++ pages/api/services/getTablaEmpleado.ts
@@ -2,13 +2,22 @@ import { PoolClient, QueryResult } from "pg";
 import pool from "../dbConfig";
 import { TableEmpleado } from "@/utils/types/dbTables";
 
-export default async function getEmpleadoData(): Promise<TableEmpleado[]> {
+export default async function getEmpleadoData(idEmpleado: string): Promise<TableEmpleado | null> {
     let client: PoolClient | null = null;
     try {
         client = (await pool.connect()) as PoolClient;
 
-        const result: QueryResult = await client.query("SELECT * FROM empleado");
-        return result.rows as TableEmpleado[];
+        const query: string = `
+            SELECT * FROM empleado
+            WHERE id_empleado = ${idEmpleado}
+            LIMIT 1
+        `;
+
+        const result: QueryResult = await client.query(query);
+        const rows = result.rows as TableEmpleado[];
+
+        // Returns found user, or user without data.
+        return rows.length > 0 ? rows[0] : null;
     } catch (err) {
         throw new Error("Failed to retrieve data from empleado.");
     } finally {
